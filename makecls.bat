32b54cde7899991e8eebd42206bae139be2d8517
Joseph Pan <cs.wzpan@gmail.com>
2013-05-03 10:20:51 +0800

    默认master分支取代basic分支，去掉basic分支

--- makecls.bat
+++
@@ -1,7 +0,0 @@
-del *.aux *.log *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps
-xelatex scnuthesis.ins
-xelatex scnuthesis.dtx
-makeindex -s gind scnuthesis
-makeindex -s gglo -o scnuthesis.gls scnuthesis.glo
-xelatex scnuthesis.dtx
-xelatex scnuthesis.dtx
